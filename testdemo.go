fe3fab73e050ea226631e363777be0eb8dfcb991
jonasbn <jonasbn@gmail.com>
2020-05-21 18:53:05 +0200

    Addressed a few minor things in the code

--- testdemo.go
+++ testdemo.go
@@ -1,5 +1,7 @@
-package test_demo
+package testdemo
 
+// Adder is just a stupid basic function where it is easy to demonstrate
+// both success and failure
 func Adder(a int, b int) int {
 	return a + b
 }
